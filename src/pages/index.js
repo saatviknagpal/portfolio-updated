13a63edc707a5b266ed3fb14a1ce6b44450a37e5
Saatvik <saatvikngpl21@gmail.com>
2023-09-08 18:21:25 -0400

    added resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
               </p>
               <div className="flex items-center self-start mt-2 lg:self-center">
                 <Link
-                  href="/dummy.pdf"
+                  href="/resume.pdf"
                   target={"_blank"}
                   className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light hover:text-dark border-2 border-solid border-transparent hover:border-dark md:p-2 md:px-4 md:text-base"
                   download={true}
